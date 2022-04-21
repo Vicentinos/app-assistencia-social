6006af70028e4a766fd8884ef798af3bc4c13c67
betofrega <637874+BetoFrega@users.noreply.github.com>
2022-04-21 19:27:43 -0300

    feat: poc authentication

--- hosting/src/main.tsx
+++ hosting/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {AuthenticationModal} from "./AuthenticationModal";
 
 ReactDOM.render(
     <React.StrictMode>
-        Hello Worlds!
+      <AuthenticationModal/>
     </React.StrictMode>,
     document.getElementById("root"),
 );
