7f1503fd2f5fde6b7c75877c259a7ea918baa2ce
betofrega <637874+BetoFrega@users.noreply.github.com>
2022-04-21 16:03:37 -0300

    chore: updating repository configurations

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,15 @@
 import * as functions from "firebase-functions";
-import { initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore"
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
-initializeApp()
+initializeApp();
 
-const db = getFirestore()
+const db = getFirestore();
 
 export const poc = functions.https.onRequest((request, response) => {
-  db.collection("poc").get().then(docs => {
-      if (!docs.empty) {
-        response.send(docs.docs[0])
-      }
-  })
+  db.collection("poc").get().then((docs) => {
+    if (!docs.empty) {
+      response.send(docs.docs[0]);
+    }
+  });
 });
