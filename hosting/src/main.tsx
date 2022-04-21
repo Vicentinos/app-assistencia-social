6fa342f823a2289b74e8b74c78ca160a760aecbb
betofrega <637874+BetoFrega@users.noreply.github.com>
2022-04-21 18:26:25 -0300

    chore: update environments

--- hosting/src/main.tsx
+++ hosting/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        Hello World!
+        Hello Worlds!
     </React.StrictMode>,
     document.getElementById("root"),
 );
