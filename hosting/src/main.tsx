1dfc9b2ad51ee9879dbfd0686fb8a945e9e73d90
betofrega <637874+BetoFrega@users.noreply.github.com>
2022-04-22 20:26:23 -0300

    chore: setup jest

--- hosting/src/main.tsx
+++ hosting/src/main.tsx
@@ -1,9 +1,8 @@
 import React, { FC } from "react";
 import ReactDOM from "react-dom";
-import { AuthenticationModal } from "./auth/AuthenticationModal";
 
 const Main: FC = () => {
-  return <AuthenticationModal />;
+  return <></>;
 };
 
 ReactDOM.render(
--- hosting/src/smoke.spec.ts
+++ hosting/src/smoke.spec.ts
@@ -1,3 +1,4 @@
+export {}; // file needs to export something under --isolatedModules
 describe("Smoke test for Jest", () => {
   it("should pass", () => {
     expect(false).not.toBe(true);
