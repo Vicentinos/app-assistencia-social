d522c72354279cecccdc753ef3501ec880cae31a
betofrega <637874+BetoFrega@users.noreply.github.com>
2022-05-09 17:33:50 -0300

    refactor: use the before hook

--- e2e-suite/cypress/integration/login/registration.spec.ts
+++ e2e-suite/cypress/integration/login/registration.spec.ts
@@ -1,9 +1,15 @@
 import { Chance } from "chance";
 
 describe("registration", () => {
-  const chance = new Chance();
-  const name = chance.name({ middle: true });
-  const email = `${name.split(" ").join(".").toLocaleLowerCase()}@example.com`;
+  let chance: Chance.Chance;
+  let name: string;
+  let email: string;
+
+  before(() => {
+    chance = new Chance();
+    name = chance.name({ middle: true });
+    email = `${name.split(" ").join(".").toLocaleLowerCase()}@example.com`;
+  });
 
   it("should register a new user", () => {
     cy.clearIndexedDB();
@@ -14,12 +20,7 @@ describe("registration", () => {
     cy.contains(
       `Enviamos um link mágico para o email ${email}. Clique no link para entrar automaticamente no App Vicentino.`
     );
-    cy.getOobCodes().should((oobCodes) => {
-      const myOobCode = oobCodes.find((oobCode) => oobCode.email === email);
-      if (!myOobCode) throw "oob code not found";
-      expect(myOobCode).to.have.property("requestType", "EMAIL_SIGNIN");
-      cy.visit(myOobCode.oobLink);
-    });
+    cy.singInFromEmailLink(email);
     cy.contains(
       `Este é o primeiro acesso com o email ${email}, que ainda não está associado a nenhuma conferência.`
     );
