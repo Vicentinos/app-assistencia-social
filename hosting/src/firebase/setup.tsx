af40e5caf68783e872c46a921b41e70deb77bdbb
betofrega <637874+BetoFrega@users.noreply.github.com>
2022-05-15 15:21:20 -0300

    fix: replace appcheck keys

--- hosting/src/firebase/setup.tsx
+++ hosting/src/firebase/setup.tsx
@@ -1,5 +1,10 @@
-import React, { PropsWithChildren, useEffect } from "react";
-import { AuthProvider, FirestoreProvider, useFirebaseApp } from "reactfire";
+import React, { PropsWithChildren } from "react";
+import {
+  AppCheckProvider,
+  AuthProvider,
+  FirestoreProvider,
+  useFirebaseApp,
+} from "reactfire";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import {
@@ -8,30 +13,29 @@ import {
 } from "firebase/app-check";
 
 const appCheckToken = import.meta.env.VITE_APP_CHECK_TOKEN || "";
+console.log(appCheckToken);
 export const FirebaseSetup: React.FC<
   PropsWithChildren<Record<string, unknown>>
 > = (props) => {
   const app = useFirebaseApp();
-  useEffect(() => {
-    if (app && import.meta.env.VITE_ENV !== "local") {
-      initializeAppCheck(app, {
-        provider: new ReCaptchaEnterpriseProvider(appCheckToken),
-        isTokenAutoRefreshEnabled: true,
-      });
-    }
-  }, [app]);
+  const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider(appCheckToken),
+    isTokenAutoRefreshEnabled: true,
+  });
+  console.log(appCheck);
   const firestoreInstance = getFirestore(app);
-
   const auth = getAuth(app);
   if (import.meta.env.VITE_ENV === "local" && !auth.emulatorConfig) {
     connectFirestoreEmulator(firestoreInstance, "localhost", 8088);
     connectAuthEmulator(auth, "http://localhost:9099");
   }
   return (
-    <AuthProvider sdk={auth}>
-      <FirestoreProvider sdk={firestoreInstance}>
-        {props.children}
-      </FirestoreProvider>
-    </AuthProvider>
+    <AppCheckProvider sdk={appCheck}>
+      <AuthProvider sdk={auth}>
+        <FirestoreProvider sdk={firestoreInstance}>
+          {props.children}
+        </FirestoreProvider>
+      </AuthProvider>
+    </AppCheckProvider>
   );
 };
--- hosting/src/firebase/useFirebaseInitJson.tsx
+++ hosting/src/firebase/useFirebaseInitJson.tsx
@@ -4,19 +4,14 @@ export function useFirebaseInitJson() {
   const [initJson, setInitJson] = useState();
   useEffect(() => {
     const abortController = new AbortController();
-    console.info("fetching init.json");
     fetch("./__/firebase/init.json", { signal: abortController.signal })
       .then((res) => {
         res.json().then((json) => {
-          console.info("fetched init.json");
           setInitJson(json);
         });
       })
-      .catch(() => {
-        console.info("fetch of init.json was aborted");
-      });
+      .catch();
     return () => {
-      console.info("aborting fetch of init.json");
       abortController.abort();
     };
   }, []);
